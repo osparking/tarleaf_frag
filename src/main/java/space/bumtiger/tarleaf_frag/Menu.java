7888b680617da742816ca43922dfb336ad9782cf
osparking <jbpark03@gmail.com>
2023-04-01 23:33:13 +0900

    menu domain class

---
+++ src/main/java/space/bumtiger/tarleaf_frag/Menu.java
@@ -0,0 +1,11 @@
+package space.bumtiger.tarleaf_frag;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Menu {
+	private String url;
+	private String label;
+}
