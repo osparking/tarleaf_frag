8c2403c0cdca770adbeda28f3d6eb6c812ebe803
osparking <jbpark03@gmail.com>
2023-04-01 22:01:34 +0900

    thymeleaf fragments example project

---
+++ src/main/java/space/bumtiger/tarleaf_frag/TarleafFragApplication.java
@@ -0,0 +1,13 @@
+package space.bumtiger.tarleaf_frag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarleafFragApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarleafFragApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bumtiger/tarleaf_frag/TarleafFragApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bumtiger.tarleaf_frag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarleafFragApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
